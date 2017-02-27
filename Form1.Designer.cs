e25850e6e8e1f3855f3c18e4ea26cccd629d37d7
swoonzj <swoonzj@verizon.net>
2017-02-27 14:46:37 -0500

    Basic Working Version
    A basic program which puts a Windows computer to sleep after a
    user-defined amount of time (in minutes).

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Sleeper
+{
+    partial class frmSleeper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSleep = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblCountdown = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTime
+            // 
+            this.txtTime.Font = new System.Drawing.Font("Cambria", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTime.Location = new System.Drawing.Point(91, 76);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(109, 86);
+            this.txtTime.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 24.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(78, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sleep in:";
+            // 
+            // btnSleep
+            // 
+            this.btnSleep.Font = new System.Drawing.Font("Cambria", 24.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSleep.Location = new System.Drawing.Point(85, 179);
+            this.btnSleep.Name = "btnSleep";
+            this.btnSleep.Size = new System.Drawing.Size(115, 46);
+            this.btnSleep.TabIndex = 2;
+            this.btnSleep.Text = "Sleep";
+            this.btnSleep.UseVisualStyleBackColor = true;
+            this.btnSleep.Click += new System.EventHandler(this.btnSleep_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Cambria", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(85, 180);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(115, 46);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Visible = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 60000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblCountdown
+            // 
+            this.lblCountdown.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCountdown.AutoSize = true;
+            this.lblCountdown.Font = new System.Drawing.Font("Cambria", 24.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountdown.Location = new System.Drawing.Point(125, 103);
+            this.lblCountdown.Name = "lblCountdown";
+            this.lblCountdown.Size = new System.Drawing.Size(35, 38);
+            this.lblCountdown.TabIndex = 5;
+            this.lblCountdown.Text = "0";
+            this.lblCountdown.Visible = false;
+            // 
+            // frmSleeper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lblCountdown);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSleep);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTime);
+            this.Name = "frmSleeper";
+            this.ShowIcon = false;
+            this.Text = "Sleeper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSleep;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblCountdown;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sleeper
+{
+    public partial class frmSleeper : Form
+    {
+        public frmSleeper()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            txtTime.Visible = true;
+            btnSleep.Visible = true; // hide buttons/labels
+            btnCancel.Visible = false; // show new button & coundown label
+            lblCountdown.Visible = false;
+
+            timer1.Enabled = false;
+        }
+
+        private void btnSleep_Click(object sender, EventArgs e)
+        {
+            txtTime.Visible = false;
+            btnSleep.Visible = false; // hide buttons/labels
+            btnCancel.Visible = true; // show new button & coundown label
+            lblCountdown.Visible = true;
+
+            lblCountdown.Text = txtTime.Text;
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int timeLeft = Convert.ToInt32(lblCountdown.Text.ToString());
+            timeLeft--;
+            lblCountdown.Text = Convert.ToString(timeLeft);
+
+            if (timeLeft == 0) // Sleep time.
+            {
+                timer1.Enabled = false;
+                btnCancel.PerformClick();
+                Application.SetSuspendState(PowerState.Suspend, false, false);
+            }
+
+
+        }
+    }
+}
